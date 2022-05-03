eeb79f656da582dacd5acb47880726abdfd5e99e
SergioWeinfortner <95246778+SergioWeinfortner@users.noreply.github.com>
2022-05-03 17:27:20 -0300

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://bodymoney-api.herokuapp.com',
+  tokenAllowedDomains: [/bodymoney-api.herokuapp.com/],
+  tokenDisallowedRoutes: [/\/oauth\/token/],
+};
+
+
